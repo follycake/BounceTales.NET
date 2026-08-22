dec3cd663cdbb1859b9b27006b89176162f7833d
follycake <sweetfollyfox@gmail.com>
2026-08-22 15:16:31 -0400

    Add raylib audio

--- BounceTales.Raylib/Program.cs
+++ BounceTales.Raylib/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using BounceTales.Platform.Software;
+using MeltySynth;
 using Raylib_cs;
 using Rl = Raylib_cs.Raylib;
 
@@ -7,6 +8,9 @@ namespace BounceTales.Raylib;
 
 internal static class Program
 {
+    public const int SampleRate = 44100;
+    public const int AudioBufferSize = 4096;
+    
     private static readonly Dictionary<KeyboardKey, KeyCode> _keyMap = new()
     {
         [KeyboardKey.Up] = KeyCode.UP,
@@ -34,11 +38,22 @@ internal static class Program
     private static void Main()
     {
         Trace.Listeners.Add(new ConsoleTraceListener());
-        using RMIDlet midlet = new();
         
         Rl.SetConfigFlags(ConfigFlags.ResizableWindow);
         Rl.InitWindow(RMIDlet.DefaultScreenWidth, RMIDlet.DefaultScreenHeight, "Bounce Tales");
         
+        Rl.SetExitKey(KeyboardKey.Delete);
+        
+        Rl.InitAudioDevice();
+        Rl.SetAudioStreamBufferSizeDefault(AudioBufferSize);
+        
+        short[] buffer = new short[2 * AudioBufferSize];
+        AudioStream stream = Rl.LoadAudioStream(SampleRate, 16, 2);
+        Rl.PlayAudioStream(stream);
+        
+        using RMIDlet midlet = new();
+        MeltySynthProvider synth = new(new Synthesizer("GeneralUser-GS.sf2", SampleRate));
+        midlet.Audio = synth;
         midlet.Start();
         while (true)
         {
@@ -46,7 +61,13 @@ internal static class Program
             midlet.Graphics.ScreenHeight = Rl.GetScreenHeight();
             if (!midlet.Update())
                 break;
-            
+
+            if (Rl.IsAudioStreamProcessed(stream))
+            {
+                synth.Sequencer.RenderInterleavedInt16(buffer);
+                Rl.UpdateAudioStream(stream, buffer, AudioBufferSize);
+            }
+
             Rl.BeginDrawing();
             SoftwareImage image = ((SoftwareGraphicsProvider)midlet.Graphics).ScreenImage;
             for (int y = 0; y < image.Height; y++)
@@ -58,7 +79,7 @@ internal static class Program
                 }
             }
             Rl.EndDrawing();
-            
+
             foreach (KeyValuePair<KeyboardKey, KeyCode> pair in _keyMap)
             {
                 if (Rl.IsKeyPressed(pair.Key))
@@ -66,10 +87,13 @@ internal static class Program
                 if (Rl.IsKeyReleased(pair.Key))
                     GameRuntime.ReleaseKey(pair.Value);
             }
-            
+
             if (Rl.WindowShouldClose())
                 midlet.RequestQuit();
         }
+        Rl.StopAudioStream(stream);
+        Rl.UnloadAudioStream(stream);
+        Rl.CloseAudioDevice();
         Rl.CloseWindow();
     }
 }
